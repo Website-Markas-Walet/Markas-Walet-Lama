4a1a9f1ff14bedabdd446ada54d927a7736c60eb
adistyharli <adistyputriharli@gmail.com>
2022-03-24 16:22:58 +0700

    commit 1
    commit

---
+++ wp-content/uploads/uag-plugin/uag-js-3444-1635080019.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){ 			window.addEventListener( 'load', function() {
+				UAGBTabs.init( '.uagb-block-03f4b4fa' );
+				UAGBTabs.anchorTabId( '.uagb-block-03f4b4fa' );
+			});
+			window.addEventListener( 'hashchange', function() {
+				UAGBTabs.anchorTabId( '.uagb-block-03f4b4fa' );
+			}, false );
+			 })
---
+++ wp-content/uploads/uag-plugin/uag-js-3444-1635080332.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){ 			window.addEventListener( 'load', function() {
+				UAGBTabs.init( '.uagb-block-c22fde52' );
+				UAGBTabs.anchorTabId( '.uagb-block-c22fde52' );
+			});
+			window.addEventListener( 'hashchange', function() {
+				UAGBTabs.anchorTabId( '.uagb-block-c22fde52' );
+			}, false );
+			 })
